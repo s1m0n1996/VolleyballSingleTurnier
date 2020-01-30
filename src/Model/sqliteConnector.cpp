7267c5a5af5fcb3551a9b78a2b7e7e2e74ed90e8
Simon Floerke <simon.floerke@googlemail.com>
2020-01-30 01:19:00 +0100

    refactoring

--- src/Model/sqliteConnector.cpp
+++ src/Model/sqliteConnector.cpp
@@ -22,13 +22,15 @@ SqliteConnector::SqliteConnector(void)
  * when the new database is created, the DDL files would execute and the schema was build.
  *
  */
-void SqliteConnector::createDatabase(const QString& path) {
+void SqliteConnector::createDatabase(const QString& path)
+{
     // TODO: checke ob datei bereits vorhanden ist und gebe eine warnung "wollen sie die datei wirklich überschreiben"
     //  aus. dann muss die Alte datei Gelöscht werden und eine neue angelegt werden"
     // TODO: logging system einführen
     _db = QSqlDatabase::addDatabase("QSQLITE");
     _db.setDatabaseName(path);
-    if (!_db.open()) {
+    if (!_db.open())
+    {
         qCritical() << "can't create or open database file on path " << path;
         return;
     }
@@ -43,8 +45,10 @@ void SqliteConnector::createDatabase(const QString& path) {
 
     QFile file;
 
-    for (const QString& scriptPath : scriptPathList) {
-        if (!QFile::exists(scriptPath)) {
+    for (const QString& scriptPath : scriptPathList)
+    {
+        if (!QFile::exists(scriptPath))
+        {
             qCritical() << "can't open script path " << scriptPath;
             return;
         }
@@ -55,9 +59,12 @@ void SqliteConnector::createDatabase(const QString& path) {
 
         QString sqlScript(stream.readAll());
         QStringList sqlStatements = sqlScript.split(";", QString::SkipEmptyParts);
-        for (const QString& sqlStatement : sqlStatements) {
-            if (sqlStatement != "") {
-                if (!query.exec(sqlStatement)) {
+        for (const QString& sqlStatement : sqlStatements)
+        {
+            if (sqlStatement != "")
+            {
+                if (!query.exec(sqlStatement))
+                {
                     qWarning() << "Failed: " << sqlStatement << "\n Reason: " << query.lastError().driverText()
                                << query.lastError().databaseText();
                 }
@@ -78,14 +85,17 @@ void SqliteConnector::createDatabase(const QString& path) {
  * This Method open an existing database.
  * If you want to open a database file that not exist, the a error was aborted the opening process.
  */
-void SqliteConnector::openDatabase(const QString& path) {
-    if (!QFile::exists(path)) {
+bool SqliteConnector::openDatabase(const QString& path)
+{
+    if (!QFile::exists(path))
+    {
         qCritical() << "can't open database file from location " << path;
         return false;
     }
     _db = QSqlDatabase::addDatabase("QSQLITE");
     _db.setDatabaseName(path);
-    if (!_db.open()) {
+    if (!_db.open())
+    {
         qCritical() << "can't create or open database file on path " << path;
         return false;
     }
@@ -103,11 +113,13 @@ void SqliteConnector::openDatabase(const QString& path) {
  * You get the requested information from the table in a matrix form (2-dimensional list).
  * If the Query fails, the function aborted with an error message and returned an empty list.
  */
-QList<QList<QVariant>> SqliteConnector::sqlQuery(const QString& sqlStatement) {
+QList<QList<QVariant>> SqliteConnector::sqlQuery(const QString& sqlStatement)
+{
 
     QSqlQuery sqlQuery;
 
-    if (!_db.open()) {
+    if (!_db.open())
+    {
         qCritical() << "database is not opened";
     }
 
@@ -131,26 +143,31 @@ QList<QList<QVariant>> SqliteConnector::sqlQuery(const QString& sqlStatement) {
  * You see that this string have 4 placeholder in form of ? char.
  * So the second parameters parameter list needs 4 elements.
  */
-QList<QList<QVariant>> SqliteConnector::sqlQuery(const QString& sqlPrepare, const QStringList& parameters) {
+QList<QList<QVariant>> SqliteConnector::sqlQuery(const QString& sqlPrepare, const QStringList& parameters)
+{
 
-    if (!_db.open()) {
+    if (!_db.open())
+    {
         qCritical() << "database is not opened";
     }
 
     QSqlQuery sqlQuery;
     // set prepare string to sqlQuery Object
-    if (!sqlQuery.prepare(sqlPrepare)) {
+    if (!sqlQuery.prepare(sqlPrepare))
+    {
         qWarning() << sqlQuery.lastError();
     }
 
     // check parameter count
-    if (parameters.count() != sqlPrepare.count("?")) {
+    if (parameters.count() != sqlPrepare.count("?"))
+    {
         qWarning() << "the number of parameters is wrong. You need " << sqlPrepare.count("?") << "in the sql string "
                    << sqlPrepare << " but in the list was " << parameters.count();
     }
 
     // add parameter to the prepare string
-    for (const QString& parameter: parameters) {
+    for (const QString& parameter: parameters)
+    {
         sqlQuery.addBindValue(parameter);
     }
 
@@ -163,20 +180,25 @@ QList<QList<QVariant>> SqliteConnector::sqlQuery(const QString& sqlPrepare, cons
  * This Function executes an sql query. If the Query fails you get a error message why it fails.
  * If the sqlQuery successful runs, you get the data in form a matrix form.
  */
-QList<QList<QVariant>> SqliteConnector::_executeQuery(QSqlQuery& sqlQueryObject, const QString& sqlQueryString) {
+QList<QList<QVariant>> SqliteConnector::_executeQuery(QSqlQuery& sqlQueryObject, const QString& sqlQueryString)
+{
 
     // if no sql string was given call the .exec() method without parameter because the parameters was
     // bind at the object before
-    if (sqlQueryString == "") {
-        if (!sqlQueryObject.exec()) {
+    if (sqlQueryString == "")
+    {
+        if (!sqlQueryObject.exec())
+        {
             qWarning() << "Failed: " << sqlQueryObject.executedQuery() << "\n Reason: "
                        << sqlQueryObject.lastError().driverText()
                        << sqlQueryObject.lastError().databaseText();
         }
     }
         // if a sql string was given call the .exec() method with this parameter
-    else {
-        if (!sqlQueryObject.exec(sqlQueryString)) {
+    else
+    {
+        if (!sqlQueryObject.exec(sqlQueryString))
+        {
             qWarning() << "Failed: " << sqlQueryObject.executedQuery() << "\n Reason: "
                        << sqlQueryObject.lastError().driverText()
                        << sqlQueryObject.lastError().databaseText();
@@ -185,11 +207,13 @@ QList<QList<QVariant>> SqliteConnector::_executeQuery(QSqlQuery& sqlQueryObject,
 
     // convert the returned data in a pretty 2-dimensional list
     QList<QList<QVariant>> data; // the complete returned table
-    while (sqlQueryObject.next()) {
+    while (sqlQueryObject.next())
+    {
 
         int column_id = 0;
         QList<QVariant> columns;
-        while (sqlQueryObject.value(column_id).isValid()) {
+        while (sqlQueryObject.value(column_id).isValid())
+        {
             columns.append(sqlQueryObject.value(column_id));
             column_id++;
         }
@@ -202,11 +226,14 @@ QList<QList<QVariant>> SqliteConnector::_executeQuery(QSqlQuery& sqlQueryObject,
  * \brief Print a table.
  * This method print a requested table in qDebug() for easier debugging.
  */
-void SqliteConnector::printTable(const QList<QList<QVariant>>& table) {
+void SqliteConnector::printTable(const QList<QList<QVariant>>& table)
+{
 
-    for (auto& row : table) {
+    for (auto& row : table)
+    {
         QString printedColumn;
-        for (auto& column : row) {
+        for (auto& column : row)
+        {
             printedColumn.append(column.toString());
             printedColumn.append(", ");
         }
