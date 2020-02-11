e2f98a56391754c339b2e0c127a56c8c9ad5e130
Lea Kesselmeier <lea.kesselmeier@gmx.de>
2020-02-11 22:06:12 +0100

    Zeigt Spieler aus Datenbank an

--- src/View/playermanagementWindow.cpp
+++ src/View/playermanagementWindow.cpp
@@ -1,5 +1,9 @@
-
+#include <QHeaderView>
 #include "playermanagementWindow.h"
+#include "Model/playermanagement.h"
+#include "Model/player.h"
+#include "QAbstractItemView"
+#include <QModelIndex>
 
 
 
@@ -9,31 +13,84 @@ PlayermanagementWindow::PlayermanagementWindow(QWidget *parent):
     ui(new Ui::PlayermanagementWindow)
 {
     ui->setupUi(this);
+    showTabel();
     createButtons();
     createAddPlayerEdit();
     createMaxPlayer();
     setAllLayout();
     connecting();
+
 }
 
 
 PlayermanagementWindow::~PlayermanagementWindow()
 {
     delete ui;
+    delete _addPlayerButton;
+    delete _startTournementButton;
+    delete _add;
+    delete _delete;
+    delete _playernameLabel;
+    delete _birthdayLabel;
+    delete _countryLabel;
+    delete _nameMaxPlayerLabel;
+    delete _valueMaxPlayerLabel;
+    delete _playernameEdit;
+    delete _birthdayEdit;
+    delete _countryEdit;
+    delete _model;
+    delete _modelGame;
+    delete _allPlayer;
+    delete _gamePlayer;
+
 }
 
-void PlayermanagementWindow:: connecting()
+void PlayermanagementWindow::connecting()
 {
-
     connect(_addPlayerButton,SIGNAL(released()), this, SLOT(addPlayer()));
     connect(_startTournementButton, SIGNAL(released()), this, SLOT(tournementName()));
+    connect(_add,SIGNAL(released()), this, SLOT(giveModel()));
 }
+QList<QList<QString>> PlayermanagementWindow::getList()
+{
+ return _listAll;
+}
+void PlayermanagementWindow::giveModel()
+{
+
+    QAbstractItemModel* model = _allPlayer->model();
+    QModelIndexList selection = _allPlayer->selectionModel()->selectedRows();
 
 
+    for (QModelIndex index : selection)
+    {
+        QList<QString> listSingel;
+
+        listSingel.append(model->index(index.row() , 0).data().toString());
+        listSingel.append(model->index(index.row() , 1).data().toString());
+        listSingel.append(model->index(index.row() , 2).data().toString());
+
+        _listAll.append(listSingel);
+    }
+
+    qDebug()<< _listAll;
+}
+void PlayermanagementWindow::showTabel()
+{
+    _model      = new PlayerManagement;
+    _allPlayer  = new TableView;
+    _gamePlayer = new TableView;
+    _allPlayer->setModel(_model->getDatabaseTableModel());
+    _allPlayer->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
+    _allPlayer->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
+    _allPlayer->horizontalHeader()->setSectionResizeMode(2, QHeaderView::Stretch);
+
+}
 void PlayermanagementWindow::tournementName()
 {
-    TournementWindow* tournementWindow = new TournementWindow;
-    tournementWindow->show();
+    TournementNamePopUp* tournementName = new TournementNamePopUp;
+    tournementName->show();
+
 }
 
 void PlayermanagementWindow::createMaxPlayer()
@@ -44,13 +101,13 @@ void PlayermanagementWindow::createMaxPlayer()
 }
 void PlayermanagementWindow::createAddPlayerEdit()
 {
-    _playernameEdit     = new WindowEdit("Max Mustermann");
-    _countryEdit        = new WindowEdit("Deutschland");
+    _playernameEdit     = new WindowEdit("Max Mustermann");    
     _birthdayEdit       = new WindowEdit("1990-01-30");
+    _countryEdit        = new WindowEdit("Deutschland");
 
-    _playernameLabel    = new WindowLabel("Spielername");
-    _countryLabel       = new WindowLabel("Land");
+    _playernameLabel    = new WindowLabel("Spielername");    
     _birthdayLabel      = new WindowLabel("Gebrurtsdatum");
+    _countryLabel       = new WindowLabel("Land");
 
 }
 
@@ -58,19 +115,10 @@ void PlayermanagementWindow::createButtons()
 {
     _addPlayerButton        = new WindowButton("Spieler hinzufügen");
     _startTournementButton  = new WindowButton("Turnier starten");
+    _add                    = new WindowButton("->");
+    _delete                 = new WindowButton("<-");
     _startTournementButton->setEnabled(false);
-    _startTournementButton->setStyleSheet("QPushButton{"
-                                          "color: #dcdcdc;"
-                                          "background-color: #40b946;"
-                                          "font-size: 20px;"
-                                          "font-family: Candara;}"
-                                          "QPushButton:disabled{"
-                                          "background-color: #acacac;}"
-                                          "QPushButton:hover{"
-                                          "color: #dcdcdc;"
-                                          "background-color: #339436;"
-                                          "font-size: 20px;"
-                                          "font-family: Candara;}");
+    _startTournementButton->setEnableStyle();
 }
 
 
@@ -79,34 +127,31 @@ void PlayermanagementWindow::setAllLayout()
     ui->maxPlayerLayout->addWidget(_nameMaxPlayerLabel,0,Qt::AlignCenter);
     ui->maxPlayerLayout->addWidget(_valueMaxPlayerLabel,0,Qt::AlignCenter);
     ui->startTournementLayout->addWidget(_startTournementButton,0 ,Qt::AlignBottom);
+
     ui->addPlayerLayout->addWidget(_playernameLabel,1,0);
-    ui->addPlayerLayout->addWidget(_countryLabel,2,0);
-    ui->addPlayerLayout->addWidget(_birthdayLabel,3,0);
+    ui->addPlayerLayout->addWidget(_birthdayLabel,2,0);
+    ui->addPlayerLayout->addWidget(_countryLabel,3,0);
+
 
     ui->addPlayerLayout->setSpacing(2);
     ui->addPlayerLayout->setMargin(5);
 
     ui->addPlayerLayout->addWidget(_playernameEdit,1,1);
-    ui->addPlayerLayout->addWidget(_countryEdit,2,1);
-    ui->addPlayerLayout->addWidget(_birthdayEdit,3,1);
+    ui->addPlayerLayout->addWidget(_birthdayEdit,2,1);
+    ui->addPlayerLayout->addWidget(_countryEdit,3,1);
+
 
     ui->addPlayerLayout->addWidget(_addPlayerButton,5,Qt::AlignRight);
+
+    ui->tabelViewLayout->addWidget(_allPlayer);
+    ui->tabelViewLayout->addWidget(_add);
+    ui->tabelViewLayout->addWidget(_gamePlayer);
+
 }
 
 void PlayermanagementWindow::setMaxPlayerLabel()
 {
-    int playerNumber= 1;
-    if (round(log(playerNumber)/log(2)) == playerNumber)
-    {
-        _valueMaxPlayerLabel->setText("0");
-        _startTournementButton->setEnabled(true);
-    }
-    else
-    {
-        int neededPlayerNumber = 4;
-        _valueMaxPlayerLabel->setText(QString::number(neededPlayerNumber));
-    }
-
+    _valueMaxPlayerLabel->setText(QString::number(_model->countMissingPlayersForNewGame()));
 }
 
 
@@ -114,5 +159,14 @@ void PlayermanagementWindow::addPlayer()
 {
     _valueMaxPlayerLabel->setText("0");
     _startTournementButton->setEnabled(true);
+
+    Player* newplayer = new Player(_playernameEdit->text(), _birthdayEdit->text(), _countryEdit->text());
+
+    _model-> addPlayerForNewGame(*newplayer);
+
+    _allPlayer->setModel(_model->getDatabaseTableModel());
+    _playernameEdit->clear();
+    _birthdayEdit->clear();
+    _countryEdit->clear();
 }
 
--- src/View/playermanagementWindow.h
+++ src/View/playermanagementWindow.h
@@ -6,11 +6,17 @@
 #include <QMainWindow>
 #include <QMessageBox>
 #include <QPushButton>
+#include <QList>
+
+
 #include "ui_playermanagementWindow.h"
 #include "windowbutton.h"
 #include "windowedit.h"
 #include "windowlabel.h"
-#include "View/tournementwindow.h"
+#include "View/tournementnamepopup.h"
+#include "Model/playermanagement.h"
+#include "View/tabelview.h"
+
 #include <cmath>
 #include <QDebug>
 
@@ -25,13 +31,18 @@ class PlayermanagementWindow : public QMainWindow
 public:
     explicit PlayermanagementWindow(QWidget *parent = nullptr);
     ~PlayermanagementWindow();
+    QList<QList<QString>> getList(void);
 
 
 signals:
 public slots:
     void addPlayer(void);
     void setMaxPlayerLabel(void);
-    void tournementName();
+    void tournementName(void);
+    void showTabel(void);
+    void highlightRow(void);
+    void giveModel(void);
+
 
 
 private slots:
@@ -43,16 +54,24 @@ private:
     void createButtons(void);
     void createMaxPlayer();
     void setAllLayout(void);
+
     Ui::PlayermanagementWindow *ui;
-    WindowButton* _addPlayerButton = nullptr;
-    WindowButton* _startTournementButton = nullptr;
-    WindowLabel* _playernameLabel = nullptr;
-    WindowLabel* _birthdayLabel = nullptr;
-    WindowLabel* _countryLabel = nullptr;
-    WindowLabel* _nameMaxPlayerLabel = nullptr;
-    WindowLabel* _valueMaxPlayerLabel = nullptr;
-    WindowEdit* _playernameEdit = nullptr;
-    WindowEdit* _birthdayEdit = nullptr;
-    WindowEdit* _countryEdit = nullptr;
+    WindowButton* _addPlayerButton          = nullptr;
+    WindowButton* _startTournementButton    = nullptr;
+    WindowButton* _add                      = nullptr;
+    WindowButton* _delete                   = nullptr;
+    WindowLabel* _playernameLabel           = nullptr;
+    WindowLabel* _birthdayLabel             = nullptr;
+    WindowLabel* _countryLabel              = nullptr;
+    WindowLabel* _nameMaxPlayerLabel        = nullptr;
+    WindowLabel* _valueMaxPlayerLabel       = nullptr;
+    WindowEdit* _playernameEdit             = nullptr;
+    WindowEdit* _birthdayEdit               = nullptr;
+    WindowEdit* _countryEdit                = nullptr;
+    PlayerManagement* _model                = nullptr;
+    PlayerManagement* _modelGame            = nullptr;
+    TableView* _allPlayer                   = nullptr;
+    TableView* _gamePlayer                  = nullptr;
+    QList<QList<QString>> _listAll;
 };
 #endif // PLAYERMANAGMENT_H
