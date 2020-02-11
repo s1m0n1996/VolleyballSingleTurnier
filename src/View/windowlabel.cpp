d82865bd212ff492a078e3f4dab5e17df161b0f8
Lea Kesselmeier <lea.kesselmeier@gmx.de>
2020-02-11 22:10:52 +0100

    Schriftgröße verändert

--- src/View/windowlabel.cpp
+++ src/View/windowlabel.cpp
@@ -3,7 +3,7 @@
 WindowLabel::WindowLabel(QString text, QWidget *parent) : QLabel(parent)
 {
     setStyleSheet("QLabel{"
-                  "font-size: 20px;"
+                  "font-size: 25px;"
                   "font-family: Candara;}");
     setText(text);
 }
