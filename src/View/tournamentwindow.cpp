3562150e18fe856313c5b151406f3599c10c3c20
Nico Meyer <nitro.meyer@t-online.de>
2020-02-25 22:27:45 +0100

    Der Spielplan wird nun generiert.

--- src/View/tournamentwindow.cpp
+++ src/View/tournamentwindow.cpp
@@ -1,124 +1,48 @@
 #include "tournamentwindow.h"
-#include <QGraphicsScene>
-#include <QGraphicsView>
 #include <QPen>
 #include <QPainter>
 #include <QGridLayout>
-#include <QLabel>
+#include <qdebug.h>
 #include <QRectF>
-
-#include "View/windowlabel.h"
-#include "View/windowbutton.h"
+#include <QLineF>
+#include <QList>
+#include <QtMath>
+#include <QIcon>
+#include <QLabel>
+#include <View/windowbutton.h>
+#include <View/windowlabel.h>
 
 TournamentWindow::TournamentWindow(QWidget *parent) : QWidget(parent)
 {
     createWidges();
-
-    QGraphicsScene* game = new QGraphicsScene();
-//    game->addRect(50, 0, 150.0, 80.0);
-//    game->addRect(50, 40,150.0, 40.0,QPen(),QBrush(Qt::red));
-
-//    game->addRect(50,100,150.0,80.0);
-//    game->addRect(50,140,150.0,40.0,QPen(),QBrush(Qt::blue));
-
-//    game->addRect(50,250,150.0,80.0);
-//    game->addRect(50,290,150.0,40.0,QPen(),QBrush(Qt::yellow));
-
-//    game->addRect(50,350,150.0,80.0);
-//    game->addRect(50,390,150.0,40.0,QPen(),QBrush(Qt::green));
-
-//    game->addRect(250,300,150.0,80.0);
-//    game->addRect(250,340,150.0,40.0,QPen(),QBrush(Qt::black));
-
-//    game->addRect(250,50,150.0,80.0);
-//    game->addRect(250,90,150.0,40.0,QPen(),QBrush(Qt::darkRed));
-
-////    game->addLine(250,60,200,100);
-//    game->addLine(200,290,225,290);//yellow mitte
-//    game->addLine(225,290,225,320);
-//    game->addLine(225,320,250,320);
-
-//    game->addLine(200,390,225,390);//green
-//    game->addLine(225,390,225,360);
-//    game->addLine(225,360,250,360);
-
-//    game->addLine(200,40,225,40);// red
-//    game->addLine(225,40,225,80);
-//    game->addLine(225,80,250,80);
-
-//    game->addLine(200,140,225,140);//blue
-//    game->addLine(225,140,225,110);
-//    game->addLine(225,110,250,110);
+    createRects();
+    createLines();
 
 
+    for(int i = 0; i < _numberOfColumn; i++)
+    {
+        for (QRectF leftSideTree : _rects[i])
+        {
+            _gameBoard->addRect(leftSideTree);
+        }
+    }
+
+    for(int i = 0; i < _numberOfColumn; i++)
+    {
+        for (QLineF lines : _lines[i])
+        {
+            _gameBoard->addLine(lines);
+        }
+    }
 
 
-    QRectF rectangle = QRectF(0, 0, 100, 50);
-
-    game->addRect(rectangle);
-    game->addText("hi");
-
-
-
-
-
-//    game->addRect(QRectF(100,100,500,200));
-
-//    double beginX = 50.0;
-//    double beginYFirstRec =  0.0;
-//    double beginYSecondRec = beginYFirstRec + 40.0;
-//    double width = 150.0;
-//    double high = 40.0;
-//    double endX = beginX + width;
-//    double endFirstLineX = endX + 25.0;
-//    double endLastLineX = endFirstLineX + 25.0;
-//    double distancePair = 60.0;
-
-//    game->addRect(beginX, beginYFirstRec, width, high * 2);
-//    game->addRect(beginX, beginYSecondRec,width, high,QPen(),QBrush(Qt::red));
-
-
-
-//    game->addRect(beginX,beginYSecondRec + distancePair,width,high);
-//    game->addRect(beginX,140,width,high,QPen(),QBrush(Qt::blue));
-
-//    game->addRect(beginX,250,width,high * 2);
-//    game->addRect(beginX,290,width,high,QPen(),QBrush(Qt::yellow));
-
-//    game->addRect(beginX,350,width,high *2);
-//    game->addRect(beginX,390,width,high,QPen(),QBrush(Qt::green));
-
-//    game->addRect(beginX + 200.0,300,width,high * 2);
-//    game->addRect(beginX + 200.0,340,width,high,QPen(),QBrush(Qt::black));
-
-//    game->addRect(beginX + 200.0,50,width,high * 2);
-//    game->addRect(beginX + 200.0,90,width,high,QPen(),QBrush(Qt::darkRed));
-
-//    game->addLine(endX,290,endFirstLineX,290);//yellow mitte
-//    game->addLine(endFirstLineX,290,endFirstLineX,320);
-//    game->addLine(endFirstLineX,320,endLastLineX,320);
-
-//    game->addLine(endX,390,endFirstLineX,390);//green
-//    game->addLine(endFirstLineX,390,endFirstLineX,360);
-//    game->addLine(endFirstLineX,360,endLastLineX,360);
-
-//    game->addLine(endX,40,endFirstLineX,40);// red
-//    game->addLine(endFirstLineX,40,endFirstLineX,80);
-//    game->addLine(endFirstLineX,80,endLastLineX,80);
-
-//    game->addLine(endX,140,endFirstLineX,140);//blue
-//    game->addLine(endFirstLineX,140,endFirstLineX,110);
-//    game->addLine(endFirstLineX,110,endLastLineX,110);
-
-    QGraphicsView* viewgame = new QGraphicsView(game);
-    viewgame->setAlignment(Qt::AlignLeft);
+    QGraphicsView* viewgameLeft = new QGraphicsView(_gameBoard);
+    viewgameLeft->setAlignment(Qt::AlignLeft);
+    // viewgameLeft->scale(1.5,1.5);
 
     QGridLayout* layout = new QGridLayout;
-    layout->addWidget(_color);
-    layout->addWidget(_title);
-    layout->addWidget(viewgame);
-    layout->addWidget(_startGame);
 
+    layout->addWidget(viewgameLeft);
     setLayout(layout);
 }
 
@@ -134,7 +58,227 @@ void TournamentWindow::createWidges()
 
     _startGame = new WindowButton("Spiel starten");
 
+}
+
+void TournamentWindow::createRects()
+{
+    // TODO: muss noch eine ANzhal an Spielrn bekommen
+    double numberOfPlayer = 32;
+    double numberOfPlayerIntern = numberOfPlayer;
+
+    // ANzahl der Spalten berechnen
+    for (int i = 0; i < numberOfPlayer; i++)
+    {
+        if (numberOfPlayerIntern > 1)
+        {
+            numberOfPlayerIntern = numberOfPlayerIntern / 2;
+            _numberOfColumn++;
+        }
+    }
+
+    // Wenn Splaten gleich 2 Sind dann soll nur ein Spiel erstellt werden.
+    if (_numberOfColumn == 2)
+    {
+        _numberOfColumn = 1;
+    }
+    else if(_numberOfColumn > 2)
+    {
+        _numberOfColumn = _numberOfColumn - 1;
+    }
+
+    _beginXRight = _beginXLeft + (_numberOfColumn * 2 + 1) * _width + (_numberOfColumn * 2) * _xSpacing;
+
+    int counter = 0;
+
+    // Liste nach Anzahl der Splaten erstellen
+    for (int i = 0; i < _numberOfColumn; i++)
+    {
+        _rects.append(QList<QRectF>());
+    }
+
+    // Die erste Spalte mit Rechtecken füllen
+    if (numberOfPlayer > 2)
+    {
+        for (int j = 0; j < numberOfPlayer / 2; j++)
+        {
+            if (counter < 2)
+            {
+                _beginYFirstRec = _beginYFirstRec + 40;
+            }
+            else
+            {
+                _beginYFirstRec = _beginYFirstRec + 140;
+                counter = 0;
+            }
+
+            _rects[0].append(QRectF(_beginXLeft, _beginYFirstRec, _width, _hight));
+            _rects[0].append(QRectF(_beginXRight, _beginYFirstRec, _width, _hight));
+
+            counter++;
+        }
+    }
+    else
+    {
+        _rects[0].append(QRectF(_beginXLeft, _beginYFirstRec, _width, _hight));
+        _rects[0].append(QRectF(_beginXLeft, _beginYFirstRec + 40, _width, _hight));
+    }
 
 
+    int counter2 = 0;
+    _beginXLeft = _beginXLeft + _xSpacing + _width;
+    _beginXRight = _beginXRight - _xSpacing - _width;
+    _beginYFirstRec = _rects[0][0].y() + _hight + 10;
+    _beginYSecondRec = 3 * _hight + 100;
+
+    // Die übrigen Spalten mit Rechtecken füllen
+    for(int i = 1; i < _numberOfColumn; i++)
+    {
+        int numberOfRect = _rects[i -1].count() / 4;
+
+        for(int k = 0; k < numberOfRect; k++)
+        {
+            if (counter2 < 2)
+            {
+                _beginYFirstRec = _beginYFirstRec + 40;
+            }
+            else
+            {
+                _beginYFirstRec = _beginYFirstRec + _beginYSecondRec + 100;
+                counter2 = 0;
+            }
+
+
+            _rects[i].append(QRectF(_beginXLeft, _beginYFirstRec, _width, _hight));
+            _rects[i].append(QRectF(_beginXRight, _beginYFirstRec, _width, _hight));
+
+            counter2++;
+
+        }
+        _beginXLeft = _beginXLeft + _xSpacing + _width;
+        _beginXRight = _beginXRight - _xSpacing - _width;
+        _beginYSecondRec = (_rects[0][0].y() + _hight + 10) * ((qPow(2,i + 2) - 2)) + 40;
+        _beginYFirstRec = _rects[0][0].y() + (3 + (i - 1) * (4 + (2 * (i - 2)))) * _hight + 10 + 100 * (qPow(2,i) - 1);
+        counter2 = 0;
+    }
+
+    // FinalSpiel hinzufügen
+    if (numberOfPlayer > 2)
+    {
+        int beginFinalGame = _rects[_numberOfColumn - 1][0].x() + ((_rects[_numberOfColumn - 1][1].x() - _rects[_numberOfColumn - 1][0].x()) / 2);
+        _rects[_numberOfColumn - 1].append(QRectF(beginFinalGame, _rects[_numberOfColumn - 1][0].y(), _width, _hight));
+        _rects[_numberOfColumn - 1].append(QRectF(beginFinalGame, _rects[_numberOfColumn - 1][2].y(), _width, _hight));
+    }
+}
+
+void TournamentWindow::createLines()
+{
+    for (int i = 0; i < _numberOfColumn; i++)
+    {
+        _lines.append(QList<QLineF>());
+    }
+
+    // Alle Splaten werden durchgegangen damit alle Rechtecke verbunden werden können
+    for (int i = 0; i < _numberOfColumn - 1; i++)
+    {
+        int numberOfRects = _rects[i].size();
+
+        double lengthLineY1 = 0;
+
+        // Linien von links die nach unten gehen
+        for (int k = 2; k < numberOfRects; k += 8)
+        {
+            if (k < numberOfRects)
+            {
+                double startLineX1Left = _rects[i][k].x() + _width;
+                double startLineY1 = _rects[i][k].y();
+                double lengthLineX1 = 50;
+                lengthLineY1 = _rects[i + 1][2].y() - _rects[i][2].y() - _hight / 2;
+
+                _lines[i].append(QLineF(startLineX1Left, startLineY1, startLineX1Left + lengthLineX1, startLineY1));
+                _lines[i].append(QLineF(startLineX1Left + lengthLineX1, startLineY1, startLineX1Left + lengthLineX1, startLineY1 + lengthLineY1));
+                _lines[i].append(QLineF(startLineX1Left + lengthLineX1, startLineY1 + lengthLineY1, startLineX1Left + lengthLineX1 + 50.0,startLineY1 + lengthLineY1));
+            }
+        }
+
+        // Linien von links die nach oben gehen
+        for (int k = 6; k < numberOfRects; k += 8)
+        {
+            if (k < numberOfRects)
+            {
+                double startLineX1 = _rects[i][k].x() + _width;
+                double startLineY1 = _rects[i][k].y();
+                double lengthLineX1 = 50;
+                lengthLineY1 = _rects[i + 1][2].y() - _rects[i][2].y() - _hight / 2;
+
+                _lines[i].append(QLineF(startLineX1, startLineY1, startLineX1 + lengthLineX1, startLineY1));
+                _lines[i].append(QLineF(startLineX1 + lengthLineX1, startLineY1, startLineX1 + lengthLineX1, startLineY1 - lengthLineY1));
+                _lines[i].append(QLineF(startLineX1 + lengthLineX1, startLineY1 - lengthLineY1, startLineX1 + lengthLineX1 + 50.0,startLineY1 - lengthLineY1));
+            }
+        }
+
+        // Linien von rechts die nach unten gehen
+        for (int k = 3; k < numberOfRects; k += 8)
+        {
+            if (k < numberOfRects)
+            {
+                double startLineX1Right = _rects[i][k].x();
+                double startLineY1 = _rects[i][k].y();
+                double lengthLineX1 = 50;
+                lengthLineY1 = _rects[i + 1][2].y() - _rects[i][2].y() - _hight / 2;
+
+                _lines[i].append(QLineF(startLineX1Right, startLineY1, startLineX1Right - lengthLineX1, startLineY1));
+                _lines[i].append(QLineF(startLineX1Right - lengthLineX1, startLineY1, startLineX1Right - lengthLineX1, startLineY1 + lengthLineY1));
+                _lines[i].append(QLineF(startLineX1Right - lengthLineX1, startLineY1 + lengthLineY1, startLineX1Right - lengthLineX1 - 50.0,startLineY1 + lengthLineY1));
+            }
+        }
+
+        // Linien von rechts die nach oben gehen
+        for (int k = 7; k < numberOfRects; k += 8)
+        {
+            if (k < numberOfRects)
+            {
+                double startLineX1Right = _rects[i][k].x();
+                double startLineY1 = _rects[i][k].y();
+                double lengthLineX1 = 50;
+                lengthLineY1 = _rects[i + 1][2].y() - _rects[i][2].y() - _hight / 2;
+
+                _lines[i].append(QLineF(startLineX1Right, startLineY1, startLineX1Right - lengthLineX1, startLineY1));
+                _lines[i].append(QLineF(startLineX1Right - lengthLineX1, startLineY1, startLineX1Right - lengthLineX1, startLineY1 - lengthLineY1));
+                _lines[i].append(QLineF(startLineX1Right - lengthLineX1, startLineY1 - lengthLineY1, startLineX1Right - lengthLineX1 - 50.0,startLineY1 - lengthLineY1));
+            }
+        }
+    }
+
+    // FinalSpiel Linien
+    // links
+    _lines[0].append(QLineF(_rects[_numberOfColumn - 1].last().x(),
+                     _rects[_numberOfColumn - 1].last().y() - _hight / 2,
+            _rects[_numberOfColumn - 1].last().x() - 50 ,
+            _rects[_numberOfColumn - 1].last().y() - _hight / 2));
+
+    _lines[0].append(QLineF(_rects[_numberOfColumn - 1].last().x() - 50,
+                     _rects[_numberOfColumn - 1].last().y() - _hight / 2,
+            _rects[_numberOfColumn - 1].last().x() - 50,
+            _rects[_numberOfColumn - 1].last().y()));
+
+    _lines[0].append(QLineF(_rects[_numberOfColumn - 1].last().x() - 50,
+                     _rects[_numberOfColumn - 1].last().y(),
+            _rects[_numberOfColumn - 1].last().x() - 50 - 125,
+            _rects[_numberOfColumn - 1].last().y()));
+    // rechts
+    _lines[0].append(QLineF(_rects[_numberOfColumn - 1].last().x() + _width,
+                     _rects[_numberOfColumn - 1].last().y() + _hight / 2,
+            _rects[_numberOfColumn - 1].last().x() + _width + 50 ,
+            _rects[_numberOfColumn - 1].last().y() + _hight / 2));
+
+    _lines[0].append(QLineF(_rects[_numberOfColumn - 1].last().x() + _width + 50,
+                     _rects[_numberOfColumn - 1].last().y() + _hight / 2,
+            _rects[_numberOfColumn - 1].last().x() + _width + 50,
+            _rects[_numberOfColumn - 1].last().y()));
+
+    _lines[0].append(QLineF(_rects[_numberOfColumn - 1].last().x() + _width + 50,
+                     _rects[_numberOfColumn - 1].last().y(),
+            _rects[_numberOfColumn - 1].last().x() + _width + 50 + 125,
+            _rects[_numberOfColumn - 1].last().y()));
 
 }
--- src/View/tournamentwindow.h
+++ src/View/tournamentwindow.h
@@ -2,6 +2,8 @@
 #define TOURNEMENTWINDOW_H
 
 #include <QWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 class WindowLabel;
 class WindowButton;
 class QLabel;
@@ -12,14 +14,37 @@ class TournamentWindow : public QWidget
 public:
     explicit TournamentWindow(QWidget *parent = nullptr);
 
-signals:
+public slots:
 
 private:
+    // TODO: Es muss von der Meldestelle übergeben werden wie viele Leute an dem Tunier teilnehmen
+
+    double _beginXLeft = 50.0;
+    double _beginXRight = 0.0;
+
+    double _width = 150.0;
+    double _hight = 40.0;
+
+    double _beginYFirstRec =  0.0;
+    double _beginYSecondRec = 0.0;
+
+    double _xSpacing = 100.0;
+    double _ySpacing = 50.0;
+
+    QList<QList<QRectF>> _rects;
+    QList<QList<QLineF>> _lines;
+
+    double _numberOfColumn = 0;
+
     void createWidges(void);
     QLabel* _color = nullptr;
     WindowLabel* _title= nullptr;
     WindowButton* _startGame= nullptr;
+    QGraphicsScene* _gameBoard = new QGraphicsScene();
 
+
+     void createRects(void);
+     void createLines(void);
 
 };
 
