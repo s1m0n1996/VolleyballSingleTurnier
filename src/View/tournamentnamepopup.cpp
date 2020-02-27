85f2825337c82edca3dc3585428c3138da3190a8
Simon Floerke <simon.floerke@googlemail.com>
2020-02-27 23:20:43 +0100

    refactoring

--- src/View/tournamentnamepopup.cpp
+++ src/View/tournamentnamepopup.cpp
@@ -1,45 +1,42 @@
 #include "tournamentnamepopup.h"
 #include "View/mainmenu.h"
 
-TournamentNamePopUp::TournamentNamePopUp(QWidget *parent) : QWidget (parent)
+TournamentNamePopUp::TournamentNamePopUp(QWidget* parent) : QWidget(parent)
 {
     _gameManagement = &GameManagement::instance();
-        setWindowTitle("Turnier erstellen");
-        setWindowIcon(QIcon(":/img/darts.png"));
+    setWindowTitle("Turnier erstellen");
+    setWindowIcon(QIcon(":/img/darts.png"));
 
-        _dateLabel = new WindowLabel("Turnierdatum");
-        _dateEdit = new WindowEdit((QDate::currentDate().toString("yyyy-MM-dd")), DataType::date);
+    _dateLabel = new WindowLabel("Turnierdatum");
+    _dateEdit = new WindowEdit((QDate::currentDate().toString("yyyy-MM-dd")), DataType::date);
 
-        _nameLabel = new WindowLabel("Turniername");
-        _nameEdit = new WindowEdit("Turniername", DataType::name);
+    _nameLabel = new WindowLabel("Turniername");
+    _nameEdit = new WindowEdit("Turniername", DataType::name);
 
-        _createTournament = new WindowButton("Erstelle Turnier");
-
-
-        QGridLayout* layout = new QGridLayout;
-
-        layout -> addWidget(_dateLabel,0,0);
-        layout -> addWidget(_dateEdit,0,1);
-        layout -> addWidget(_nameLabel,1,0);
-        layout -> addWidget(_nameEdit,1,1);
-        layout -> addWidget(_createTournament,2,2);
-        setLayout(layout);
-
-        connect(_createTournament, SIGNAL(released()), this, SLOT(openTournamentWindow()));
+    _createTournament = new WindowButton("Erstelle Turnier");
 
 
+    QGridLayout* layout = new QGridLayout;
 
-        setWindowFlags(Qt::WindowStaysOnTopHint|Qt::WindowCloseButtonHint);
+    layout->addWidget(_dateLabel, 0, 0);
+    layout->addWidget(_dateEdit, 0, 1);
+    layout->addWidget(_nameLabel, 1, 0);
+    layout->addWidget(_nameEdit, 1, 1);
+    layout->addWidget(_createTournament, 2, 2);
+    setLayout(layout);
 
+    connect(_createTournament, SIGNAL(released()), this, SLOT(openTournamentWindow()));
+
+    setWindowFlags(Qt::WindowStaysOnTopHint | Qt::WindowCloseButtonHint);
 }
 
 
 void TournamentNamePopUp::openTournamentWindow(void)
 {
-     setWindowFlags(Qt::Window);
-     QString tournamentName = _nameEdit->text();
-     QDate tournamentDate = QDate::fromString(_dateEdit->text(), "yyyy-MM-dd");
+    setWindowFlags(Qt::Window);
+    QString tournamentName = _nameEdit->text();
+    QDate tournamentDate = QDate::fromString(_dateEdit->text(), "yyyy-MM-dd");
 
-     _gameManagement->createNewTournament(tournamentName, tournamentDate);
-     _gameManagement->loadOtherTournament(tournamentName, tournamentDate);
+    _gameManagement->createNewTournament(tournamentName, tournamentDate);
+    _gameManagement->loadOtherTournament(tournamentName, tournamentDate);
 }
