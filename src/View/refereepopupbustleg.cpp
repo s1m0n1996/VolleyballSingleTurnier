03504d190e4d67760bece1e273303712840b7d6b
Lea Kesselmeier <lea.kesselmeier@gmx.de>
2020-03-04 09:36:33 +0100

    Button und Schrift Layout verändert

--- src/View/refereepopupbustleg.cpp
+++ src/View/refereepopupbustleg.cpp
@@ -8,24 +8,34 @@
 
 RefereePopupBustLeg::RefereePopupBustLeg(QWidget *parent) : QWidget(parent)
 {
+    setWindowTitle("Richteransicht");
+    setWindowIcon(QIcon(":/img/referee.png"));
 
-    _refereeInformation = new WindowLabel("Der Spieler hat siche überworfen oder nicht mit einem Doppel beendet.");
-    _refereeAction = new WindowLabel("Wenn der Wurf richtig eingegeben wurde dann auf den Button 'Nächster Spieler' klicken"
-                                     " ansonsten auf den Button 'Undo letzter Wurf'");
+    _refereeInformation = new WindowLabel("Der Spieler hat sich überworfen oder nicht mit einem Doppel beendet");
+    _refereeInformation->setTitleStyle();
+    _refereeAction = new WindowLabel("Wenn der Wurf richtig eingegeben wurde,\n"
+                                     "dann auf den Button 'Nächster Spieler' klicken\n"
+                                     "ansonsten auf den Button 'Letzter Wurf rückgänig'");
 
-    _naechsterSpieler = new WindowButton("Nächster Spieler");
-    _undoLastThrow = new WindowButton("UNDO letzter Wurf");
+    _nextPlayer = new WindowButton("Nächster Spieler");
+    _undoLastThrow = new WindowButton("Letzter Wurf rückgänig");
 
 
     QGridLayout* layout = new QGridLayout;
+    QHBoxLayout* buttonLayout = new QHBoxLayout;
+
+
+    buttonLayout -> addWidget(_nextPlayer);
+    buttonLayout -> addWidget(_undoLastThrow);
+
 
     layout -> addWidget(_refereeInformation,0,0);
     layout -> addWidget(_refereeAction,1,0);
-    layout -> addWidget(_naechsterSpieler,2,0);
-    layout -> addWidget(_undoLastThrow,2,1);
+    layout -> addLayout(buttonLayout,2,1);
+
     setLayout(layout);
 
-    connect(_naechsterSpieler, SIGNAL(released()), this, SLOT(openRefereeBustLeg()));
+    connect(_nextPlayer, SIGNAL(released()), this, SLOT(openRefereeBustLeg()));
     connect(_undoLastThrow, SIGNAL(released()), this, SLOT(openRefereeUndoLastThrow()));
     setWindowFlags(Qt::SubWindow);
 }
